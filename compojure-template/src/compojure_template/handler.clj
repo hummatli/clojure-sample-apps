4f0c239d842fe967de4295cafbf69f03ddac1624
Sattar Hummatli <settarxan@gmail.com>
2018-01-30 17:22:11 +0400

    add compojure sample app to test clojure in web

---
+++ compojure-template/src/compojure_template/handler.clj
@@ -0,0 +1,11 @@
+(ns compojure-template.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World 12")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
