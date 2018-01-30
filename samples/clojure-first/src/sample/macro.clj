c9f8a911369204ed280aed1012fedb507dad7bf1
Sattar Hummatli <settarxan@gmail.com>
2018-01-30 20:32:54 +0400

    add sample for macros

--- samples/clojure-first/src/sample/macro.clj
+++ samples/clojure-first/src/sample/macro.clj
@@ -1,3 +1,13 @@
 (ns sample.macro)
 
 (println (macroexpand-1 '(when true (println "hello"))))
+
+
+;Treading macro
+(println
+  (macroexpand-1
+    '(-> (a 1)
+         (b 2)
+         (c 3)
+         (d 4))
+    ))
